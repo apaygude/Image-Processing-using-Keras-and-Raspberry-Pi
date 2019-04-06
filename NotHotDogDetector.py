118e26dcd8bfad4964374c98dfafe43202ec3199
apaygude <38838923+apaygude@users.noreply.github.com>
2019-04-05 19:21:41 -0500

    Add files via upload

---
+++ NotHotDogDetector.py
@@ -0,0 +1,81 @@
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+from imutils.video import VideoStream
+from threading import Thread
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+
+# define the paths to the Not Hotdog Keras deep learning model and
+MODEL_PATH = "/home/pi/hotdog_not_hotdog.model"
+
+ 
+# initialize the total number of frames that *consecutively* contain
+TOTAL_CONSEC = 0
+ 
+# initialize is the Hotdog alarm has been triggered
+#HOTDOG = False
+# load the model
+print("[INFO] loading model...")
+model = load_model(MODEL_PATH)
+ 
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=350)
+	# prepare the image to be classified by our deep learning network
+	image = cv2.resize(frame, (28, 28))
+	image = image.astype("float") / 255.0
+	image = img_to_array(image)
+	image = np.expand_dims(image, axis=0)
+
+	# classify the input image and initialize the label and
+	# probability of the prediction
+	(notHotdog, hotdog) = model.predict(image)[0]
+	label = "Not Hotdog"
+	proba = notHotdog
+# check to see if hotdog was detected using our convolutional
+	# neural network
+	if hotdog > notHotdog:
+            
+		# update the label and prediction probability
+		label = "Hotdog"
+		proba = hotdog
+ 
+		# increment the total number of consecutive frames that
+		# contain hotDog
+		TOTAL_CONSEC += 1
+		
+
+ 
+# otherwise, reset the total number of consecutive frames
+	else:
+
+		TOTAL_CONSEC = 0
+# build the label and draw it on the frame
+	label = "{}: {:.2f}%".format(label, proba * 100)
+	frame = cv2.putText(frame, label, (10, 25),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+ 
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+ 
+# do a bit of cleanup
+print("[INFO] cleaning up...")
+cv2.destroyAllWindows()
+vs.stop()
